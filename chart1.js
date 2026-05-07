cb457f1005f074c7cee9b4e2bdfed7c7abc6edb6
Squiggle9105 <khoyt7168@gmail.com>
2026-05-07 14:33:37 -0700

    declaring todayOz to 0 before setting to a cookie

--- chart1.js
+++ chart1.js
@@ -16,13 +16,13 @@ var yesterday = null;
 // will hold the day before yesterday (or ereyesterday) as a String
 var ereyesterday = null;
 
+// will store oz drank in one day
+var ozDrank = 0;
 // base cookie for ozDrank today
 
 document.cookie = setCookie("todayOz",ozDrank);
 
-// will store oz drank in one day
-var ozDrank = getCookie("todayOz"); 
-
+ozDrank = getCookie("todayOz"); 
 
 // if today isn't Sunday, yesterday is one spot backward in the array. If today is Sunday, yesterday is at the end of the array.
 if (todayDate.getDay() > 0)
