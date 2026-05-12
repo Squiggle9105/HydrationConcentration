68300c642c963494bc2e428c6eba95f4be963185
Squiggle9105 <khoyt7168@gmail.com>
2026-05-12 13:59:33 -0700

    chaning notif message

--- notifications.js
+++ notifications.js
@@ -5,14 +5,15 @@ function notifyMe() {
   } else if (Notification.permission === "granted") {
     // Check whether notification permissions have already been granted;
     // if so, create a notification
-    const notification = new Notification("Hi there!");
+    var msg = "Remember to stay hydrated! You've had " + getOzDrank + " ounces so far today!";
+    const notification = new Notification(msg);
     // …
   } else if (Notification.permission !== "denied") {
     // We need to ask the user for permission
     Notification.requestPermission().then((permission) => {
       // If the user accepts, let's create a notification
       if (permission === "granted") {
-        const notification = new Notification("Hi there!");
+        const notification = new Notification(msg);
         // …
       }
     });
